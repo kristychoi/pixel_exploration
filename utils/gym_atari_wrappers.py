8023179db5e2a15c38ae4da7d0073c14a6c1f622
kristychoi <kristy.choi24@gmail.com>
2018-03-07 17:40:02 -0800

    bankheist fix; wip

--- utils/gym_atari_wrappers.py
+++ utils/gym_atari_wrappers.py
@@ -114,7 +114,10 @@ class MaxAndSkipEnv(gym.Wrapper):
 
 
 def _process_frame84(frame):
-    img = np.reshape(frame, [210, 160, 3]).astype(np.float32)
+    if frame.shape == (210, 160, 3):
+        img = frame.astype(np.float32)
+    else:
+        img = np.reshape(frame, [210, 160, 3]).astype(np.float32)
     img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
     resized_screen = cv2.resize(img, (84, 110),  interpolation=cv2.INTER_LINEAR)
     x_t = resized_screen[18:102, :]
